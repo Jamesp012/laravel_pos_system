ee1ec1d5a887177c493f4c07121d523796a4db85
JamesP012 <jamespatrickurquiza@gmail.com>
2024-11-19 07:59:19 +0800

    Before Adding Datatables

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return view('index');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard'); // Assuming you have a "dashboard.blade.php" view
+})->name('dashboard');
